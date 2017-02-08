ed3f09ff8e6c897fdf58c6c4cef0f931a9e07b46
John Karahalis <john.karahalis@gmail.com>
2017-02-08 13:41:20 -0500

    Working legend

--- distributionviewer/core/static/js/app/components/views/chart-list.js
+++ distributionviewer/core/static/js/app/components/views/chart-list.js
@@ -3,27 +3,50 @@ import { Link } from 'react-router';
 
 import ChartContainer from '../containers/chart-container';
 import DescriptionContainer from '../containers/description-container';
+import LegendContainer from '../containers/legend-container';
 
 
 export default function(props) {
+  let chartLinks = [];
+  let firstMetricId;
+  props.metadata.map(metricMeta => {
+    if (firstMetricId === undefined) firstMetricId = metricMeta.id;
+
+    const tooltip = <DescriptionContainer rawDescription={metricMeta.description} asTooltip={true} />;
+    chartLinks.push(
+      <Link key={metricMeta.id} className="chart-link" to={`/chart/${metricMeta.id}/`}>
+        <div>
+          <ChartContainer
+            metricId={metricMeta.id}
+            isDetail={false}
+            showOutliers={false}
+            tooltip={tooltip}
+            whitelistedPopulations={props.whitelistedPopulations}
+          />
+        </div>
+      </Link>
+    );
+  });
+
+  let maybeLegendContainer;
+  if (props.whitelistedPopulations.length > 1) {
+    maybeLegendContainer = (
+      <LegendContainer
+        // In chart listings, all charts show the same populations, so it
+        // doesn't really matter which one the legend subscribes to.
+        metricId={firstMetricId}
+
+        whitelistedPopulations={props.whitelistedPopulations}
+      />
+    );
+  }
+
   return (
-    <section className="chart-list">
-      {props.metadata.map(metricMeta => {
-        const tooltip = <DescriptionContainer rawDescription={metricMeta.description} asTooltip={true} />;
-        return (
-          <Link key={metricMeta.id} className="chart-link" to={`/chart/${metricMeta.id}/`}>
-            <div>
-              <ChartContainer
-                metricId={metricMeta.id}
-                isDetail={false}
-                showOutliers={false}
-                tooltip={tooltip}
-                whitelistedPopulations={props.whitelistedPopulations}
-              />
-            </div>
-          </Link>
-        );
-      })}
-    </section>
+    <article id="chart-list">
+      {maybeLegendContainer}
+      <section className="charts">
+        {chartLinks}
+      </section>
+    </article>
   );
 }
